9820223110ad683ea17e1e66f8c67aac37eb3eb4
parthhhx <79368045+parthhhx@users.noreply.github.com>
2023-03-08 17:19:51 +0530

    Update func.py

--- func.py
+++ func.py
@@ -1,42 +1,48 @@
-from flask import Flask, request, jsonify
-from PIL import Image
-import io
+from flask import Flask, render_template, request
 import cv2
 import time
 
 app = Flask(__name__)
 
 
-@app.route('/convert', methods=['POST'])
-def convert_image():
-    # Get the uploaded low-resolution image from the request
-    file = request.files['image']
-    img = Image.open(file)
+def increase_resolution(image_path):
+    img = cv2.imread(image_path)
 
     width = img.shape[1]
     height = img.shape[0]
     bicubic = cv2.resize(img, (width*4, height*4))
-    # Call your Python function to generate a high-resolution image
-    # high_res_img = generate_high_resolution_image(img)
-
-    # Convert the high-resolution image to bytes for sending back in the response
 
     super_res = cv2.dnn_superres.DnnSuperResImpl_create()
+
     super_res.readModel('ESPCN_x4.pb')
     super_res.setModel('espcn', 4)
     espcn_image = super_res.upsample(img)
 
     super_res.readModel('FSRCNN_x4.pb')
     super_res.setModel('fsrcnn', 4)
-    high_res_img_bytes = super_res.upsample(img)
+    fsrcnn_image = super_res.upsample(img)
+    # Apply the algorithm to increase the resolution
+    # You would need to define the algorithm that suits your purpose
 
-    high_res_img_bytes = io.BytesIO()
-    high_res_img_bytes.save(high_res_img_bytes, format='PNG')
-    high_res_img_bytes.seek(0)
-
-    # Send the high-resolution image back in the response
-    return jsonify({'image': high_res_img_bytes.read().decode('latin-1')})
+    # Return the high-resolution image
+    return fsrcnn_image
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Get the user's input (the low-resolution image)
+        low_res_img = request.files['low_res_img'].read()
+
+        # Apply the algorithm to increase the resolution
+        high_res_img = increase_resolution(low_res_img)
+
+        # Return the high-resolution image to the user
+        return render_template('result.html', high_res_img=high_res_img)
+    else:
+        return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
